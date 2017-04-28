c070d0b60f142d7ff4b7752d3fccf32ea7e52c83
a91566.Xiaobao <a91566@163.com>
2017-04-28 22:20:42 +0800

    柳庭风静人眠昼

---
+++ wfa_20170428/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace wfa_20170428
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+			this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+			this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+			this.toolStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Location = new System.Drawing.Point(45, 204);
+			this.button1.Margin = new System.Windows.Forms.Padding(4);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(123, 49);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "创建工具条";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Location = new System.Drawing.Point(238, 204);
+			this.button2.Margin = new System.Windows.Forms.Padding(4);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(123, 49);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "开启拖拽";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button3.Location = new System.Drawing.Point(406, 204);
+			this.button3.Margin = new System.Windows.Forms.Padding(4);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(123, 49);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "关闭拖拽";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button4.Location = new System.Drawing.Point(565, 204);
+			this.button4.Margin = new System.Windows.Forms.Padding(4);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(123, 49);
+			this.button4.TabIndex = 4;
+			this.button4.Text = "遍历";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripTextBox1,
+            this.toolStripDropDownButton1});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 322);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(739, 25);
+			this.toolStrip1.TabIndex = 5;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// toolStripDropDownButton1
+			// 
+			this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+			this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+			this.toolStripDropDownButton1.Size = new System.Drawing.Size(29, 22);
+			this.toolStripDropDownButton1.Text = "toolStripDropDownButton1";
+			// 
+			// toolStripLabel1
+			// 
+			this.toolStripLabel1.Name = "toolStripLabel1";
+			this.toolStripLabel1.Size = new System.Drawing.Size(96, 22);
+			this.toolStripLabel1.Text = "toolStripLabel1";
+			// 
+			// toolStripTextBox1
+			// 
+			this.toolStripTextBox1.Name = "toolStripTextBox1";
+			this.toolStripTextBox1.Size = new System.Drawing.Size(100, 25);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(739, 347);
+			this.Controls.Add(this.toolStrip1);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+		private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+		private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+	}
+}
+
---
+++ wfa_20170428/Form1.cs
@@ -0,0 +1,141 @@
+﻿/*
+ * 2017年4月28日22:11:17	郑少宝
+ * 
+ * 我喜欢夏天里阳光中的雨
+ * 冬天里雾气中的阳光
+ * 和任何时候的你
+ */
+using System;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace wfa_20170428
+{
+	public partial class Form1 : Form
+	{
+		private ToolStrip _ts;
+		public Form1()
+		{
+			InitializeComponent();
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.StartPosition = FormStartPosition.CenterScreen;
+			this.FormBorderStyle = FormBorderStyle.FixedSingle;
+			this.button1_Click(null,null);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			_ts = new ToolStrip();
+			_ts.Parent = this;
+			_ts.Height = 58;
+			_ts.AutoSize = false;
+			_ts.ImageScalingSize = new Size(48,48);
+			this.addButtons();
+			button1.Enabled = false;
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			openCloseDrag(true);
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			openCloseDrag(false);
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			StringBuilder sb = new StringBuilder();
+			foreach (ToolStripItem item in _ts.Items)
+			{
+				sb.Append($"{ item.ToolTipText}{ Environment.NewLine}");
+			}
+			MessageBox.Show(sb.ToString());
+		}
+		
+
+		private void addButtons()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				ToolStripItem tsi;
+				if (i % 2 == 0)
+				{
+					tsi = new ToolStripDropDownButton();
+				}
+				else
+				{
+					tsi = new ToolStripButton();
+				}
+				tsi.DisplayStyle = ToolStripItemDisplayStyle.Image;
+				tsi.Image = this.getImage(i);
+				tsi.ToolTipText = $"Number{ i }";
+				tsi.Text = tsi.ToString();
+				tsi.AllowDrop = true;
+				tsi.MouseDown += (s, e) => this.DoDragDrop(s, DragDropEffects.Move);
+				tsi.DragEnter += (s, e) => e.Effect = DragDropEffects.Move;
+				tsi.DragDrop += (s, e) =>
+				{
+					ToolStripItem source = e.Data.GetData(typeof(ToolStripButton)) as ToolStripItem;
+					//不知道为何不能用 e.Data.GetData(typeof(ToolStripItem)) 
+					if (source == null)
+					{
+						source = e.Data.GetData(typeof(ToolStripDropDownButton)) as ToolStripItem;
+					}
+					//Point point = _ts.PointToClient(new Point(e.X, e.Y));
+					//ToolStripItem target = _ts.GetItemAt(point);
+					this.moveLocation(source, s as ToolStripItem);
+				};
+			
+				_ts.Items.Add(tsi);
+			}
+		}
+
+		private void moveLocation(ToolStripItem source, ToolStripItem target)
+		{
+			if (source == null || target == null) return;
+			_ts.Items.Insert(_ts.Items.IndexOf(target), source);
+		}
+
+		private Image getImage(int i)
+		{
+			switch (i)
+			{
+				case 0:
+					return Properties.Resources.Number0;
+				case 1:
+					return Properties.Resources.Number1;
+				case 2:
+					return Properties.Resources.Number2;
+				case 3:
+					return Properties.Resources.Number3;
+				case 4:
+					return Properties.Resources.Number4;
+				case 5:
+					return Properties.Resources.Number5;
+				case 6:
+					return Properties.Resources.Number6;
+				case 7:
+					return Properties.Resources.Number7;
+				case 8:
+					return Properties.Resources.Number8;
+				case 9:
+					return Properties.Resources.Number9;
+			}
+			return null;
+		}
+
+		private void openCloseDrag(bool isopen)
+		{
+			foreach (ToolStripItem tsb in this._ts.Items)
+			{
+				tsb.AllowDrop = isopen;
+			}
+
+		}
+				
+	}
+}
